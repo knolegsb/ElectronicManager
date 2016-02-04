f0325ac83373cad4864348a0d24cc9b7249d7f39
knolegsb <seon2you@hotmail.com>
2016-02-04 11:36:10 -0800

    structure

---
+++ ElectronicManager.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElectronicManager.Web.MvcApplication" Language="C#" %>
---
+++ ElectronicManager.Web/Infrastructure/DepartmentDb.cs
@@ -0,0 +1,31 @@
+﻿using ElectronicManager.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ElectronicManager.Web.Infrastructure
+{
+    public class DepartmentDb : DbContext, IDepartmentDataSource
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        IQueryable<Employee> IDepartmentDataSource.Employees
+        {
+            get
+            {
+                return Employees;
+            }
+        }
+
+        IQueryable<Department> IDepartmentDataSource.Departments
+        {
+            get
+            {
+                return Departments;
+            }
+        }
+    }
+}
